4ffa1b848a68b485f479b2866cd44ad28a73fcfe
iingjeong <silvmin42@gmail.com>
2026-07-30 17:45:56 +0900

    feat: MydataKey, MydataRiaAccount 도메인 DTO 추가

---
+++ src/main/java/com/app/mydata/domain/mydata/dto/MydataKeyDTO.java
@@ -0,0 +1,11 @@
+package com.app.mydata.domain.mydata.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter @ToString @Builder
+
+public class MydataKeyDTO {
+    private String ciHash;
+}
