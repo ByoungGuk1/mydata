729d0ae19f3e8a1f4400bda3fae578dc1afd1ced
iingjeong <silvmin42@gmail.com>
2026-08-04 12:59:04 +0900

    feat: mydata RiaAccount/Trade 도메인 NotFoundException 추가

--- src/main/java/com/app/mydata/domain/mydata/exception/MydataRiaAccountException.java
+++ src/main/java/com/app/mydata/domain/mydata/exception/MydataRiaAccountException.java
@@ -1,8 +1,5 @@
 package com.app.mydata.domain.mydata.exception;
 
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
 public class MydataRiaAccountException extends RuntimeException {
 
     public MydataRiaAccountException(String message) {
--- src/main/java/com/app/mydata/domain/mydata/exception/MydataTradeException.java
+++ src/main/java/com/app/mydata/domain/mydata/exception/MydataTradeException.java
@@ -1,8 +1,5 @@
 package com.app.mydata.domain.mydata.exception;
 
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
 public class MydataTradeException extends RuntimeException {
 
     public MydataTradeException(String message) {
