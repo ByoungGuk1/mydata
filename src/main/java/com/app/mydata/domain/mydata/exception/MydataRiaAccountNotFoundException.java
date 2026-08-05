b07e597ca1ece72eafde7114c56c9fb0da7281a0
iingjeong <silvmin42@gmail.com>
2026-08-05 16:49:33 +0900

    refactor: NotFoundException이 대응 Exception을 상속하도록 계층 통일

--- src/main/java/com/app/mydata/domain/mydata/exception/MydataRiaAccountNotFoundException.java
+++ src/main/java/com/app/mydata/domain/mydata/exception/MydataRiaAccountNotFoundException.java
@@ -1,6 +1,6 @@
 package com.app.mydata.domain.mydata.exception;
 
-public class MydataRiaAccountNotFoundException extends RuntimeException {
+public class MydataRiaAccountNotFoundException extends MydataRiaAccountException {
 
     public MydataRiaAccountNotFoundException(String message) {
         super(message);
